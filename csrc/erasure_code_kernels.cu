470cd3bdba944e752e779bcc7d6369888aea8312
CharlesFang207 <CharlesFang207@outlook.com>
2023-11-19 20:15:53 +0800

    add kernel dispatching to erasure code encoding part

--- csrc/erasure_code_kernels.cu
+++ csrc/erasure_code_kernels.cu
@@ -10,6 +10,14 @@
 
 namespace vllm {
 
+template <typename scalar_t, typename int_t>
+__device__ scalar_t bitwise_xor(scalar_t a, scalar_t b) {
+    int_t a_int = *reinterpret_cast<int_t*>(&a);
+    int_t b_int = *reinterpret_cast<int_t*>(&b);
+    int_t result_int = a_int ^ b_int;
+    return *reinterpret_cast<scalar_t*>(&result_int);
+}
+
 template <typename scalar_t>
 __global__ void xor_kernel(
     scalar_t *dst,
@@ -17,7 +25,7 @@ __global__ void xor_kernel(
     const int64_t block_size_in_bytes) {
     const int64_t idx = blockIdx.x * blockDim.x + threadIdx.x;
     if (idx < block_size_in_bytes / sizeof(scalar_t)) {
-        dst[idx] ^= src[idx];
+        dst[idx] = bitwise_xor<scalar_t, int>(dst[idx], src[idx]);
     }
 }
 
@@ -52,7 +60,14 @@ void xor_and_swap_out(
     const cudaStream_t stream = at::cuda::getCurrentCUDAStream();
 
     cudaMemcpyPeer(dst_1_ptr, dst_1_device.index(), src_1_ptr, src_1_device.index(), block_size_in_bytes);
-    vllm::xor_kernel<scalar_t><<<(block_size_in_bytes + 1024 - 1) / 1024, 1024, 0, stream>>>(dst_1_ptr, src_2_ptr, block_size_in_bytes);
+
+    VLLM_DISPATCH_FLOATING_TYPES(
+    src_1.scalar_type(), "xor_kernel", ([&] {
+      vllm::xor_kernel<scalar_t><<<(block_size_in_bytes + 1024 - 1) / 1024, 1024, 0, stream>>>(
+        reinterpret_cast<scalar_t *>(src_1_ptr), 
+        reinterpret_cast<scalar_t *>(src_2_ptr), 
+        block_size_in_bytes);
+    }));
 
     cudaMemcpyAsync(dst_2_ptr, dst_1_ptr, block_size_in_bytes, memcpy_type, stream);
 
